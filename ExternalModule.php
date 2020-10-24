60da0397a645efcab21b014825ea722bb2685511
romanoe <noemi.romano@protonmail.com>
2020-10-24 12:14:37 +0200

    change API services, store bbox

--- ExternalModule.php
+++ ExternalModule.php
@@ -11,12 +11,8 @@ use \REDCap as REDCap;
  */
 class ExternalModule extends AbstractExternalModule
 {
-
-  // get_class($this)->exitAfterHook();
-
     public function redcap_survey_page_top($project_id, $record = null, $instrument, $event_id, $group_id = null, $survey_hash, $response_id = null, $repeat_instance = 1)
     {
-
         if ($instrument == 'adresse_nonexistante') {
             $this->loadOpenLayers();
         }
@@ -25,22 +21,11 @@ class ExternalModule extends AbstractExternalModule
 
     public function redcap_survey_complete($project_id, $record = null, $instrument, $event_id, $survey_hash, $group_id = null, $response_id, $repeat_instance = 1)
     {
-
-
         if ($instrument == 'mon_lieu_dhabitation') {
-
             $this -> addressValidation($record, $event_id);
-
-
-
-
-
         } elseif ($instrument == 'adresse_nonexistante') {
             $this -> manualGeocoding($record, $event_id);
-
         }
-
-
     }
 
 
@@ -74,27 +59,38 @@ class ExternalModule extends AbstractExternalModule
         //Get data from Address form
         $npa = $_POST['shz_npa'];
         $address =  $_POST['shz_address'];
-        $address_nr =  $_POST['shz_strname_nr'];
+        $city = $_POST['shz_ville'];
+
+        //Get bbox
+        $bbox_url = "https://api3.geo.admin.ch/rest/services/api/MapServer/find?layer=ch.swisstopo-vd.ortschaftenverzeichnis_plz&sr=2056&searchText=" . $npa . "&searchField=plz";
+        $bbox_data_array = file_get_contents(str_replace(' ', '%20', $bbox_url));
+        $bbox_results = json_decode(utf8_encode($bbox_data_array)) -> results;
+        $bbox = implode(",", $bbox_results[0]->bbox);
 
 
-        //Replace space by %20
-        $address_url = str_replace(' ', '%20', $address);
+        //Get address
+        $geographic_data_url = "https://api3.geo.admin.ch/rest/services/api/SearchServer?layer=ch.bfs.gebaeude_wohnungs_register&searchText=" . $address .  "&type=locations&origins=address&sr=2056&bbox=" . $bbox ;
+        $geographic_data_array = file_get_contents(str_replace(' ', '%20', $geographic_data_url));
+        $geographic_results = json_decode(utf8_encode($geographic_data_array)) -> results ;
 
-        // Url
-        $geographic_data_url = "https://lasigvm2.epfl.ch/api/complete_address/?deinr=" . $address_nr . "&search=" . $npa . "%20" .  $address_url ;
 
-        //Call API
-        $geographic_data_array = file_get_contents($geographic_data_url);
-        $geographic_data = json_decode(utf8_encode($geographic_data_array));
+        //Get bbox city
+        $city_data_url = "https://api3.geo.admin.ch/rest/services/api/SearchServer?searchText=" . $city ."&origins=gg25&type=locations&sr=2056";
+        $city_data_array = file_get_contents(str_replace(' ', '%20', $city_data_url));
+        $city_results = json_decode(utf8_encode($city_data_array)) -> results ;
+        $city_bbox = str_replace(" ", ",", substr($city_results[0] -> attrs -> geom_st_box2d, 4, -1));
 
 
         //Validate address
-        if (count($geographic_data)==1) {
+        if (count($geographic_results)==1) {
 
-            // Get egid and X, Y coordinates
-            $egid = $geographic_data[0] -> egid;
-            $gkode = $geographic_data[0] -> gkode;
-            $gkodn = $geographic_data[0] -> gkodn;
+
+
+            //Get X, Y coordinates
+            $gkode = $geographic_results[0]-> attrs -> y;
+            $gkodn = $geographic_results[0]-> attrs -> x;
+
+
 
 
             //Save egid, XY coordinates and validate address
@@ -103,8 +99,8 @@ class ExternalModule extends AbstractExternalModule
                               'redcap_event_name' => REDCap::getEventNames(true, true, $event_id),
                               'gkode' => $gkode,
                               'gkodn' => $gkodn,
-                              'egid' => $egid,
-                              'addr_is_valid' => 1
+                              'addr_is_valid' => 1,
+                              'bbox' => "[" . $city_bbox . "]"
                             );
 
             // Encode to json
@@ -119,21 +115,20 @@ class ExternalModule extends AbstractExternalModule
             $data_to_save = array(
                               'record_id' => $record,
                               'redcap_event_name' => REDCap::getEventNames(true, true, $event_id),
-                              'addr_is_valid' => 0
+                              'addr_is_valid' => 0,
+                              'bbox' => "[" . $city_bbox . "]"
                             );
 
             // Encode to json
             $data_json = json_encode(array($data_to_save));
 
             //Save data
+            //redirect(REDCap::getSurveyLink($record, 'adresse_nonexistante', $event_id));
             REDCap::saveData('json', $data_json);
 
+
             return redirect(REDCap::getSurveyLink($record, 'adresse_nonexistante', $event_id));
-
-
-
         }
-
     }
 
 
@@ -161,33 +156,21 @@ class ExternalModule extends AbstractExternalModule
 
         // Save data
         return REDCap::saveData('json', $data_json);
-
-
     }
 
 
+    // Redirects to URL provided using PHP, and if
+    public function redirect($url)
+    {
 
-
-
-
-    function redirect($url)
-  {
-     // If contents already output, use javascript to redirect instead
-     if (headers_sent())
-     {
-       // $this -> exitAfterHook();
-        exit("<script type=\"text/javascript\">window.location.href=\"$url\";</script>");
-
-     }
-     // Redirect using PHP
-     else
-     {
-        // $this -> exitAfterHook();
-        header("Location: $url");
-
-     }
-  }
-
-
-
+       // If contents already output, use javascript to redirect instead
+        if (headers_sent()) {
+            exit("<script type=\"text/javascript\">window.location.href=\"$url\";</script>");
+        }
+        // Redirect using PHP
+        else {
+            header("Location: $url");
+            exit;
+        }
+    }
 }
