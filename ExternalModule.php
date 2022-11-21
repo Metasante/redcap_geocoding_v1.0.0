e1b5b4932ab3e423838cbc2b88f52af29593212d
Marco <marco.vieiraruas@epfl.ch>
2022-11-21 17:44:14 +0100

    config + externalModule changes

--- ExternalModule.php
+++ ExternalModule.php
@@ -53,89 +53,108 @@ class ExternalModule extends AbstractExternalModule
      * @param string $record
      *  The record name.
      */
-    public function addressValidation($record, $event_id)
-    {
+     public function addressValidation($record, $event_id)
+   {
 
-        //Get data from Address form
-        $npa = $_POST['shz_npa'];
-        $address =  $_POST['shz_address'];
-        $city = $_POST['shz_ville'];
+       //Get data from Address form
+       $npa = $_POST['shz_npa'];
+       $address =  $_POST['shz_address'];
+       $city = $_POST['shz_ville'];
 
-        //Get bbox
-        $bbox_url = "https://api3.geo.admin.ch/rest/services/api/MapServer/find?layer=ch.swisstopo-vd.ortschaftenverzeichnis_plz&sr=2056&searchText=" . $npa . "&searchField=plz";
-        $bbox_data_array = file_get_contents(str_replace(' ', '%20', $bbox_url));
-        $bbox_results = json_decode(utf8_encode($bbox_data_array)) -> results;
-        $bbox = implode(",", $bbox_results[0]->bbox);
+       //Get bbox
+       $bbox_url = "https://api3.geo.admin.ch/rest/services/api/MapServer/find?layer=ch.swisstopo-vd.ortschaftenverzeichnis_plz&sr=2056&searchText=" . $npa . "&searchField=plz";
+       $bbox_data_array = file_get_contents(str_replace(' ', '%20', $bbox_url));
+       $bbox_results = json_decode(utf8_encode($bbox_data_array)) -> results;
+
+       $big_bbox = [3000000, 3000000, -3000000, -3000000];
+
+       foreach ($bbox_results as $b) {
+           if($big_bbox[0]>$b->bbox[0]){
+               $big_bbox[0]=$b->bbox[0];
+           }
+           if($big_bbox[1]>$b->bbox[1]){
+               $big_bbox[1]=$b->bbox[1];
+           }
+           if($big_bbox[2]<$b->bbox[2]){
+               $big_bbox[2]=$b->bbox[2];
+           }
+           if($big_bbox[3]<$b->bbox[3]){
+               $big_bbox[3]=$b->bbox[3];
+           }
+       }
+
+       $bbox = implode(",", $big_bbox);
+
+       //Get address
+       $geographic_data_url = "https://api3.geo.admin.ch/rest/services/api/SearchServer?layer=ch.bfs.gebaeude_wohnungs_register&searchText=" . $address .  "&type=locations&origins=address&sr=2056&bbox=" . $bbox ;
+       $geographic_data_array = file_get_contents(str_replace(' ', '%20', $geographic_data_url));
+       $geographic_results = json_decode(utf8_encode($geographic_data_array)) -> results ;
+
+       if ($city == 'Cheseaux-Noréaz,Yverdon-les-Bains') {
+           $city = 'Yverdon-les-Bains';
+       }
+       elseif ($city =='Prilly,Jouxtens-Mézery') {
+                $city = 'Prilly';
+       }
+
+       //Get bbox city
+       $city_data_url = "https://api3.geo.admin.ch/rest/services/api/SearchServer?searchText=" . $city ."&origins=gg25&type=locations&sr=2056";
+       $city_data_array = file_get_contents(str_replace(' ', '%20', $city_data_url));
+       $city_results = json_decode(utf8_encode($city_data_array)) -> results ;
+       $city_bbox = str_replace(" ", ",", substr($city_results[0] -> attrs -> geom_st_box2d, 4, -1));
+
+       //Check if user entered address number
+       $is_there_any_number = preg_match('~[0-9]+~', substr($address,-3));
+
+       //Validate address
+       if (count($geographic_results) > 0 && $is_there_any_number) {
 
 
-        //Get address
-        $geographic_data_url = "https://api3.geo.admin.ch/rest/services/api/SearchServer?layer=ch.bfs.gebaeude_wohnungs_register&searchText=" . $address .  "&type=locations&origins=address&sr=2056&bbox=" . $bbox ;
-        $geographic_data_array = file_get_contents(str_replace(' ', '%20', $geographic_data_url));
-        $geographic_results = json_decode(utf8_encode($geographic_data_array)) -> results ;
 
-
-        //Get bbox city
-        $city_data_url = "https://api3.geo.admin.ch/rest/services/api/SearchServer?searchText=" . $city ."&origins=gg25&type=locations&sr=2056";
-        $city_data_array = file_get_contents(str_replace(' ', '%20', $city_data_url));
-        $city_results = json_decode(utf8_encode($city_data_array)) -> results ;
-        $city_bbox = str_replace(" ", ",", substr($city_results[0] -> attrs -> geom_st_box2d, 4, -1));
-
-
-        //Check if user entered address number
-        $is_there_any_number = preg_match('~[0-9]+~', substr($address,-3));
-
-
-        //Validate address
-        if (count($geographic_results) > 0 && $is_there_any_number) {
+           //Get X, Y coordinates
+          $gkode = $geographic_results[0]-> attrs -> y;
+           $gkodn = $geographic_results[0]-> attrs -> x;
 
 
 
+           //Save egid, XY coordinates and validate address
+           $data_to_save = array(
+                             'record_id' => $record,
+                             'redcap_event_name' => REDCap::getEventNames(true, true, $event_id),
+                             'gkode' => $gkode,
+                             'gkodn' => $gkodn,
+                             'addr_is_valid' => 1,
+                             'bbox' => "[" . $city_bbox . "]"
+                           );
 
-            //Get X, Y coordinates
-            $gkode = $geographic_results[0]-> attrs -> y;
-            $gkodn = $geographic_results[0]-> attrs -> x;
+           // Encode to json
+           $data_json = json_encode(array($data_to_save));
 
+           //Save data
+           return REDCap::saveData('json', $data_json);
+       } else {
 
+           // Redirect to survey link of adresse_nonexistante instrument
+           //Save egid, XY coordinates and validate address
+           $data_to_save = array(
+                             'record_id' => $record,
+                             'redcap_event_name' => REDCap::getEventNames(true, true, $event_id),
+                             'addr_is_valid' => 0,
+                             'bbox' => "[" . $city_bbox . "]",
+                             'mon_adresse_complete' => 0
+                           );
 
+           // Encode to json
+           $data_json = json_encode(array($data_to_save));
 
-            //Save egid, XY coordinates and validate address
-            $data_to_save = array(
-                              'record_id' => $record,
-                              'redcap_event_name' => REDCap::getEventNames(true, true, $event_id),
-                              'gkode' => $gkode,
-                              'gkodn' => $gkodn,
-                              'addr_is_valid' => 1,
-                              'bbox' => "[" . $city_bbox . "]"
-                            );
+           //Save data
+           //redirect(REDCap::getSurveyLink($record, 'adresse_nonexistante', $event_id));
+           REDCap::saveData('json', $data_json);
 
-            // Encode to json
-            $data_json = json_encode(array($data_to_save));
-
-            //Save data
-            return REDCap::saveData('json', $data_json);
-        } else {
-
-            // Redirect to survey link of adresse_nonexistante instrument
-            //Save egid, XY coordinates and validate address
-            $data_to_save = array(
-                              'record_id' => $record,
-                              'redcap_event_name' => REDCap::getEventNames(true, true, $event_id),
-                              'addr_is_valid' => 0,
-                              'bbox' => "[" . $city_bbox . "]",
-                              'mon_adresse_complete' => 0
-                            );
-
-            // Encode to json
-            $data_json = json_encode(array($data_to_save));
-
-            //Save data
-            //redirect(REDCap::getSurveyLink($record, 'adresse_nonexistante', $event_id));
-            REDCap::saveData('json', $data_json);
-
-            return $this->redirect(REDCap::getSurveyLink($record, 'adresse_nonexistante', $event_id));
-            //return redirect(REDCap::getSurveyLink($record, 'adresse_nonexistante', $event_id));
-        }
-    }
+           return $this->redirect(REDCap::getSurveyLink($record, 'adresse_nonexistante', $event_id));
+           //return redirect(REDCap::getSurveyLink($record, 'adresse_nonexistante', $event_id));
+       }
+   }
 
 
 
